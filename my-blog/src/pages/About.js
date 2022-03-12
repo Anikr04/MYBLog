fdf2099b3aebcd83b05762163d4403d6d45f1bd5
Anikr04 <73298015+Anikr04@users.noreply.github.com>
2022-03-12 23:12:05 +0530

    Add files via upload

---
+++ my-blog/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About=()=>{
+  return (
+    <>
+     <div className='mb-20'>
+    <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 textgray-900'>About</h1>
+    <p className='mx-auto leading-relaxed text-base mb-4'> welcome to my Blog! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur culpa exercitationem laborum vitae. Officiis dolor amet necessitatibus, illum nihil perferendis aliquam, deleniti asperiores sit culpa, vero maiores maxime quos. Cum!</p>
+    <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium iste possimus soluta temporibus dolores fugit accusamus expedita unde? Saepe repellat ipsum laudantium est maxime sequi, officia veritatis animi voluptates. Assumenda consequatur modi facilis dolor aspernatur iusto, ipsa officiis, sequi dolorum similique placeat quo vel eveniet, nesciunt corporis earum deleniti.</p>
+    <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo molestiae nihil, modi aut ducimus ex minima quisquam eius earum in corrupti iusto, dolores natus suscipit aliquid consectetur esse et dolore? Reiciendis, architecto aspernatur? Officia sapiente sed possimus facilis esse? Corporis minima voluptate repellendus facere quis tempore dolores delectus quasi ut?</p>
+    </div>
+    </>
+  )
+}
+
+export default About;
